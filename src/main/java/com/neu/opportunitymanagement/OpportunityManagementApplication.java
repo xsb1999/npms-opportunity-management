721a87dd4ee56351b2edcd4b433d8465d949e2f3
xsb1999 <18810235625@163.com>
2021-07-19 23:37:33 +0800

    注册到eureka - provider

--- src/main/java/com/neu/opportunitymanagement/OpportunityManagementApplication.java
+++ src/main/java/com/neu/opportunitymanagement/OpportunityManagementApplication.java
@@ -2,8 +2,12 @@ package com.neu.opportunitymanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableEurekaClient
 public class OpportunityManagementApplication {
 
     public static void main(String[] args) {
